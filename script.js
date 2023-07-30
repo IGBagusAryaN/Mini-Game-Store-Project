4bc52302e069635b43b52c4f866acc51b6790ff3
Bagus Arya <aryabagus453@gmail.com>
2023-07-30 17:51:04 +0800

    first push

--- script.js
+++ script.js
@@ -13,3 +13,65 @@ if (close) {
         nav.classList.remove('active')
     })
 }
+
+
+// carousel
+const carousel = document.querySelector('.card-container');
+const arrowBtns = document.querySelectorAll('.wrapper i');
+const firstCardWidth = carousel.querySelector('.card').offsetWidth;
+const carouselChildrens = [...carousel.children]
+
+let isDragging = false, startX, startScrollLeft;
+
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML)
+})
+
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML)
+})
+
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+    });
+});
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft
+}
+
+const dragging = (e) => {
+    if(!isDragging) return;
+    carousel.scrollLeft = e.pageX;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+};
+
+const infiniteScroll = () => {
+    if(carousel.scrollLeft === 0) {
+        carousel.classList.add("no-transition")
+        carousel.scrollLeft = carousel.scrollWidth - ( 2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition")
+    }
+    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+        carousel.classList.add("no-transition")
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition")
+    }
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);

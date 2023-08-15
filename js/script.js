2d3ded3dadf2444e22e0b4d533e630ff93243d43
Bagus Arya <aryabagus453@gmail.com>
2023-08-16 02:07:32 +0800

    first push

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
-
-if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active')
-    })
-}
-
-if (close) {
-    close.addEventListener('click', () => {
-        nav.classList.remove('active')
-    })
-}
 
 // hero
   let lists = document.querySelectorAll('.item');
